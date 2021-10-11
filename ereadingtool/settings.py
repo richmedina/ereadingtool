16c4a22a2d762ddcea1d4fca2e565cfae4edc63d
Richard Medina <rmedina@hawaii.edu>
2021-10-11 10:10:07 -1000

    add star tld to settings

--- ereadingtool/settings.py
+++ ereadingtool/settings.py
@@ -121,6 +121,9 @@ ALLOWED_HOSTS = ['0.0.0.0',
                  'admin.steps2ar.org',
                  'api.steps2advancedreading.org',
                  'admin.steps2advancedreading.org',
+                 'star.flagshipconnect.org',
+                 'api.star.flagshipconnect.org',
+                 'admin.star.flagshipconnect.org',
 ]
 
 
